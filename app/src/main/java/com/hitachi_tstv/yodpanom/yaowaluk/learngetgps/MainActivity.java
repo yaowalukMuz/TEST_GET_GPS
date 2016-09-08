c70048ed7d305ceba5d900120f5c746e88efd1cf
yaowamuz <yaowamuz.c@gmail.com>
2016-09-08 12:10:36 +0700

    add class SQLite

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/learngetgps/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/learngetgps/MainActivity.java
@@ -13,8 +13,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -33,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private int timeAnInt = 0;
 
 
+    //Explicit SQLite
+    private StampGPS objStampGPS;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +48,66 @@ public class MainActivity extends AppCompatActivity {
 
         setupLocation();
 
+        connectedSQLite();
+
+    }// onCreate
+
+    private void connectedSQLite(){
+        objStampGPS = new StampGPS(this);
     }
+
+    public void clickSaveData(View view) {
+
+        String strStamp = plateEditText.getText().toString().trim();
+
+        if (strStamp.equals("")) {
+            Toast.makeText(this, "กรุณากรอกชื่อ สถานที่ด้วย คะ",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+
+            String strLat = latTextView.getText().toString();
+            String strLng = lngTextView.getText().toString();
+
+            updateValueToSQLite( strLat, strLng,strStamp);
+
+        }
+
+    }   // clickSaveData
+
+
+    public void updateValueToSQLite( String strLat, String strLng,String strStamp) {
+        objStampGPS.addTransactionGPS(strLat, strLng, strStamp);
+        finish();
+
+    }
+
+//    private void updateValueToSQLite(String strName, String strLat, String strLng) {
+//
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//        RequestBody requestBody = new FormEncodingBuilder()
+//                .add("isAdd", "true")
+//                .add("Name", strName)
+//                .add("Lat", strLat)
+//                .add("Lng", strLng)
+//                .build();
+//        Request.Builder builder = new Request.Builder();
+//        Request request = builder.url("http://swiftcodingthai.com/watch/php_add_plate.php").post(requestBody).build();
+//        Call call = okHttpClient.newCall(request);
+//        call.enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Request request, IOException e) {
+//
+//            }
+//
+//            @Override
+//            public void onResponse(Response response) throws IOException {
+//                finish();
+//            }
+//        });
+//
+//
+//    }   // updateValue
+
 
     private void autoUpdate() {
 
@@ -55,7 +120,9 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(threadPolicy);
 
         //Get Current Time
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+       // DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
         Date date = new Date();
         String getTimeDate = dateFormat.format(date);
 
@@ -73,19 +140,18 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 autoUpdate();
             }
-        }, 1000);   // Mil Second
+        }, 10);   // Mil Second
     }
-
-
-
 
     @Override
     protected void onResume() {
         super.onResume();
-        //locationManager.removeUpdates(locationListener);
-        locationManager.removeUpdates(locationListener);
-        String strLat = "Unknow";
-        String strLng = "Unknow";
+
+
+        // locationManager.removeUpdates(locationListener);
+
+        String strLat = "Unknown";
+        String strLng = "Unknown";
 
         Location networkLocation = requestLocation(LocationManager.NETWORK_PROVIDER, "No Internet");
         if (networkLocation != null) {
@@ -99,18 +165,52 @@ public class MainActivity extends AppCompatActivity {
             strLng = String.format("%.7f", gpsLocation.getLongitude());
         }
 
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        String getTimeDate = dateFormat.format(date);
+
         latTextView.setText(strLat);
         lngTextView.setText(strLng);
+        plateEditText.setText(getTimeDate.toString());
+
+    }   // onResume
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GPSABoolean = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!GPSABoolean) {
+            networkABoolean = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!networkABoolean) {
+                Log.d("GPS", "Cannot Find Location");
+            }
+
+        }
+
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locationManager.removeUpdates(locationListener);
 
     }
-
 
     public Location requestLocation(String strProvider, String strError) {
 
@@ -119,6 +219,16 @@ public class MainActivity extends AppCompatActivity {
         if (locationManager.isProviderEnabled(strProvider)) {
 
 
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
             locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
             location = locationManager.getLastKnownLocation(strProvider);
 
@@ -156,21 +266,21 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    private void setupLocation() {
 
-
-
-
-    private void bindWidget(){
-        latTextView = (TextView) findViewById(R.id.textView5);
-        lngTextView = (TextView) findViewById(R.id.textView6);
-        plateEditText = (EditText) findViewById(R.id.editText);
-    }
-
-    private void setupLocation(){
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE );
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         criteria = new Criteria();
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
         criteria.setAltitudeRequired(false);
         criteria.setBearingRequired(false);
+
+
+    }   // setupLocation
+
+    private void bindWidget() {
+        latTextView = (TextView) findViewById(R.id.textView4);
+        lngTextView = (TextView) findViewById(R.id.textView6);
+        plateEditText = (EditText) findViewById(R.id.editText);
     }
+
 }
